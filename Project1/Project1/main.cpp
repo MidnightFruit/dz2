bc4179fd7e0d935c08a258f1e43726a723f0b843
Dmitriy <dima.gladkikh.2016@mail.ru>
2021-04-05 00:17:26 +0700

    Дз для 2 занятия

---
+++ Project1/Project1/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+enum xando
+{
+	x, o, _
+};
+struct xo
+{
+	xando pole[2][2];
+};
+
+
+int main()
+{
+	int x = 0;
+	char y = 'a';
+	float z = 1.5;
+	double f = 2.5;
+	xando pole[2][2];
+	return 0;
+}
